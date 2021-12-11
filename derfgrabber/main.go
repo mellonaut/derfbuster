d9993e95078b8ecd2c51bc7f634e7bb5b3c7972b
mellonaut <65114647+mellonaut@users.noreply.github.com>
2021-12-11 14:29:54 -0500

    Pseudocode
    Sort of laying out ideas looking at Baked's script

--- derfgrabber/main.go
+++ derfgrabber/main.go
@@ -15,3 +15,16 @@ func main() {
 
 // fmt.Println("[!] ERROR")
 // fmt.Println("No input parameters provided")
+// set global vars from results of previous gobuster,feroxbuster,etc
+// ip=
+// check_http=
+// check_https= 
+// check_port=
+	
+// function for checking input
+	// check each host in the scan results for $web_ports or $web_portsplus (including other popular/easily exploitable services known to run on weird ports
+	// if $port is found, dial that port to verify host is up
+	// create directory for target in the "loot" folder
+	// banner grab / timestamp / something to bookmark a kickoff and store
+
+	
