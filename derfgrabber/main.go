4af44baf24c2931b4c8e52ef3fcdbda9f99cee95
mellonaut <rleeazw@outlook.com>
2021-12-11 18:29:02 -0500

    This is pretty cool

--- derfgrabber/main.go
+++ derfgrabber/main.go
@@ -3,23 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 )
 
-func main() {
-	f, err := os.Open("/test.txt")
+var current_version = "0.0.1"
+var title = "Derfgrabber"
+
+// Function for no input values	
+func novalues() {
+	fmt.Println("[!] ERROR")
+	fmt.Println("No input parameters provided, big dawg. Try again, with heart this time.")
+}
+
+
+func crash() {
+	f, err := os.Open("/crashlog.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println(f.Name(), "Opened a can...")
 
+// Version function
+func version() {
+    fmt.Println(current_version)
+}
+// Title function
+func display_title() {
+	fmt.Println(title)
+|
 
-// function for no input values	
-// fmt.Println("[!] ERROR")
-// fmt.Println("No input parameters provided")
-	
-// Help, Options, examples, versions functions
-	
+
+// Exampoles
 func examples() {
 	fmt.Println("Examples:")
 	fmt.Println("\"$ derfgrabber -s http://10.10.10.10 -f <gobusterfile> -e gb\"")
@@ -28,22 +43,55 @@ func examples() {
 	fmt.Println("\"$ derfgrabber -s https://10.10.10.10:4443 -f <ffuf csv file> -e ff\"")
 }
 
+
+// Help
+func help(){
+    version
+    fmt.Println("")    
+	fmt.Println("Param:      Description:")
+    fmt.Println("------      ------------")
+    fmt.Println("-h          help")
+    fmt.Println("-v          version")
+    fmt.Println("-s[svr]     http(s) server to grab screen shots from")
+    fmt.Println("-f[file]    Output file from which ever enumeration tool was used")
+    fmt.Println("-e[value]   Input file enumeration type. Supported types:")
+    fmt.Println("                - Feroxbuster - \"fb\"")
+    fmt.Println("                - Gobuster: \"gb\"")
+    fmt.Println("                - Ffuf: \"ff\"")
+    fmt.Println("            Supported ffuf output formats: json, csv, or md ONLY")
+    fmt.Println("            NOTE: you must have jq installed for json formated files to parse")
+    examples
+}
+
+
+// Options, 
+
+
+	
+
 	
 	
 // function for taking input options arguments etc
-// derf has a while/do here, but I'm not sure how to do that in Go
-// h) help
-// v) version
-// s) server address
-// f) enumeration file
-// e) enumeration file type
-// P) Polite mode - will show preview and ask for confirmation
-// \?) Invalid Options
+func options() {
+	h := flag.Bool("h", false, "Get some help.")
+	V := flag.Bool("V", false, "Get the current version.")
+	v := flag.Bool("v", false, "Verbose mode.")
+	s := flag.String("s", "", "HTTP(S) server to grab screen shots from.")
+	f := flag.String("f", "", "Output file from which ever enumeration tool was used.")
+	e := flag.String("e", "", "Input file enumeration type. Supported types:")
+	nr := flag.Bool("nr", false, "Suppress report opening in browser.")
+	p := flag.Bool("p", false, "Polite mode enabled")
+	P := flag.Bool("p", false, "Very Polite mode enabled")
+	\? := flag.Bool("\?", false, "Get some help.")
+}
+
 // If no options given send to help
 // if [[ $server_param == "" ]]; then
 //     novalues
 //     exit 1
-	
+	flag.Parse()
+	args := flag.Args()
+
 // set global vars from results of previous gobuster,feroxbuster,etc
 // ip=
 // check_http=
@@ -117,12 +165,49 @@ func review(){
 	        // dump screenshots, source dumps
 	      // check and signal all tasks done signal to close goroutine
 	    // generate html report from results, zip screenshots and raw files with just the banner grab and the report
-	      
+
+	func report() {
+			f, err := os.Open("/$folder_name/aggregated.html")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(f.Name(), "Opening report in browser...")
+
 	// If using Optional attached DB or mounted volume can do something for that here
 		  
 	// If using Optional attached message broker / queue can do something for that here
 		 
 	// Pass off to middleware / API Handler module for processing
 	
-
+// Main Function 
+// How do I do Polite mode? Run main but without report? How do I rate limit and do review?
+func main() {
+	// Check for input parameters, validate
+	check_input $server_param $file_param $enum_type
+	// Set global vars from input parameters
+	create_vars $server_param $file_param $enum_type
+	// fmt.Println(title + version)
+	title
 	
+	// Display review / Verify Actions if Polite mode
+	review $server_param $file_param $enum_type
+
+	// create target directories in $loot folder
+	create_directories 
+	// Grab banner grab and store in $loot/$folder_name/
+	banner_grab
+	// Grab screenshots and store in $loot/$folder_name/$agg_site
+	grab_screen_shots_and_aggregate $server_param
+
+	// Pass off to middleware / API Handler module for processing
+	hand_off
+
+	fmt.Println("")
+	fmt.Println("[+] Aggregated Report saved to ./$folder_name/$agg_site")
+
+	// Open report in browser
+	report
+}
+
+
